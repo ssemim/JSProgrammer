3229cdc8ae8c53343b667a26fa46f6f5a04e5707
SSEMIM <82812894+ssemim@users.noreply.github.com>
2024-11-26 14:26:23 +0900

    Update 조건문.js

--- 조건문.js
+++ 조건문.js
@@ -16,4 +16,21 @@ function rank (a) {
 
 rank(score);
 
-//
+// 2884번 알람 시계
+
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let hour = Number(input[0].split(' ')[0]);
+let minute = Number(input[0].split(' ')[1]);
+
+if(minute<45){ // 45분 미만일시
+    hour -= 1;
+    minute += 15;
+    if(hour<0) hour = 23;
+}
+else minute -= 45;
+
+console.log(hour+" "+minute);
+
+// 
